b06c1eda9999dd3ff7a5cd93bcffcc608ae1f2aa
Святослав <example@example.ru>
2023-02-28 16:34:13 +0300

    modified yellow circles

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 import random
 
+from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QButtonGroup
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QInputDialog
-from yellow_circle import Ui_MainWindow
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication
 
 
-class Example(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('yellow_circle.ui', self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -27,12 +27,12 @@ class Example(QMainWindow, Ui_MainWindow):
 
     def draw_flag(self, qp):
         r = random.randint(1, 50)
-        qp.setBrush(QColor(255, 229, 2))
+        qp.setBrush(QColor(random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)))
         qp.drawEllipse(random.randint(1, 200), random.randint(1, 200), r, r)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
+    ex = MyWidget()
     ex.show()
     exit(app.exec())
